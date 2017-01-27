ed3435237c6258f1225a9d663da1fe314d4b78d8
Richard McGain <richard.mcgain@shopify.com>
2017-01-27 16:45:27 -0500

    write to an artifacts dotdir

--- lib/fastcov.rb
+++ lib/fastcov.rb
@@ -8,7 +8,8 @@ module Fastcov
       Coverage.start
       at_exit do
         result = Coverage.result
-        File.write("/tmp/fastcov-#{Process.pid}.json", result.to_json)
+        Dir.mkdir('.artifacts') unless Dir.exists?('.artifacts')
+        File.write(".artifacts/fastcov-#{Process.pid}.json", result.to_json)
       end
     end
   end
