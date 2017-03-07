184815a61e6a828831b139187b1651c989f8d7a0
Richard McGain <richard.mcgain@shopify.com>
2017-03-07 18:30:38 +1100

    ugh

--- lib/fastcov.rb
+++ lib/fastcov.rb
@@ -8,8 +8,8 @@ module Fastcov
       Coverage.start
       at_exit do
         result = Coverage.result
-        Dir.mkdir('.artifacts') unless Dir.exists?('.artifacts')
-        File.write(".artifacts/fastcov-#{Process.pid}.json", result.to_json)
+        Dir.mkdir('log') unless Dir.exists?('log')
+        File.write("log/fastcov-#{Process.pid}.json", result.to_json)
       end
     end
   end
